f7b2a0e11990adc8fb78380b9eb6318d3443d9ff
Andrew Ruder <andy@aeruder.net>
2004-08-26 05:57:58 +0000

    Reorganization of the directory/file structure
    git-svn-id: file:///Users/andy/svnstuff/trustees/trunk@10 6c2541cd-f2e0-0310-a9c1-f72c7e98ba2a

--- include/security.h
+++
@@ -1,2 +0,0 @@
-int trustees_init_security(void);
-void trustees_deinit_security(void);
--- include/trustees_private.h
+++ include/trustees_private.h
@@ -1,3 +1,5 @@
+int trustees_init_security(void);
+void trustees_deinit_security(void);
 /* 
  *  (c) 1999-2000 Vyacheslav Zavadsky
  *  GPLed
@@ -70,3 +72,28 @@ extern int sys_set_trustee(const struct trustee_command * c);
 
 
 
+/*
+ * Trustees ACL Project 
+ *
+ * Copyright (c) 2004 Andrew Ruder (aeruder@ksu.edu) 
+ *
+ *	This program is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU General Public License as
+ *	published by the Free Software Foundation, version 2.
+ *
+ * Shared components of the fs
+ *
+ * History:
+ * 
+ */
+
+/*
+ * Magic number!
+ * 
+ * FIXME: Do I just make this up or is there some system for coming
+ * up with magic numbers?
+ */
+#define TRUSTEES_MAGIC 0x32236975
+
+int trustees_init_fs(void);
+void trustees_deinit_fs(void);
