6d6aa41cb1bb677782c815994a0ea057c86d4999
Andrew Ruder <andy@aeruder.net>
2004-11-06 07:49:20 +0000

    Resolving some todo list stuff Did testing on bad inputs (in the config file). The settrustees will attempt to auto-find trustees device file -major- code cleanup in settrustees.c
    git-svn-id: file:///Users/andy/svnstuff/trustees/trunk@72 6c2541cd-f2e0-0310-a9c1-f72c7e98ba2a

--- src/settrustees.c
+++ src/settrustees.c
@@ -3,7 +3,6 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
-#include <trustees.h> 
 #include <linux/limits.h>
 #include <grp.h>
 #include <sys/types.h>
@@ -14,283 +13,377 @@
 #include <sys/sysmacros.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
-char *trustee_device = "/etc/trustees/trustees";
+#include "trustees.h" 
+
+char *trustee_device = NULL;
+char *trustee_config = "/etc/trustees.conf";
 
 int set_trustee(const struct trustee_command * command)
 {
-    int procfd;
-    /* syscall_number==0 means to use /proc/trustee/syscall for write */
-	procfd = open (trustee_device, O_WRONLY);
-    if (procfd < 0) return errno;
-    if (write (procfd,command,sizeof(*command)) != sizeof(*command))
-      return errno;
-    close (procfd);
-    return 0;
+	int fd;
+	int ret;
+
+	fd = open (trustee_device, O_WRONLY);
+
+	if (fd < 0) return errno;
+
+	ret = write(fd, command, sizeof(struct trustee_command));
+	if (ret != sizeof(struct trustee_command)) return errno;
+
+	close (fd);
+
+	return 0;
 }
  
-void print_exit(void) {
+char *determine_trustees_mount(void) {
+	FILE *file;
+	char x[10000];
+	char *device;
+	char *mount;
+	char *fstype;
+	char *options;
+	char *num1;
+	char *num2;
 
-	printf("Usage: settrustee <options>\n");
-	printf("Options:\n");
-	printf("        -f <trustee info file name> see fromat below. Default /etc/trustee.conf.\n");
-	printf("        -D delete all trustees from the kernel and exit\n");
-	printf("        -d delete all trustees from the kernel before processing the trustees in the file\n");
-	printf("        -t <trustees device file> specify the 'trustees' file from the mounted trusteefs fs\n");
-	printf("        -p prefix to file names\n");
-	printf("File format:\n");
-	printf("A set of string like:\n");
-	printf("[block_device_name]/path/path:user_name:DCRWBE:+group_name:OtherChars\n");
-	printf("{network_name}/path/path:user_name:DCRWBE:+group_name:OtherChars\n");
-	
-	printf("The string is started from the file (or directory) name. Double // is not allowed\n");
-	printf("The next field is user name (* means everybody) or + followed by group name\n");
-	printf("The next field is trustee mask. Possible chars DCRWBEUX mean:\n");
-	printf("      R  - Read files permission\n");
-	printf("      W  - Write permission\n");
-	printf("      B  - Browse (like execute permission for directories)\n");
-	printf("      E  - rEad diriectories\n");
-	printf("      X  - eXecute (files, granted only if owner can execute the file\n");
-	printf("      U  - use Unix permission system (set as default for /)");
-	printf("      D  - deny the permissions in mask\n");
-	printf("      C  - clear the permissions in mask\n");
-	printf("      O  - only the directory and files in it (not subdirectories) affected by the trustee\n");
-	printf("      !  - All except user (group) affected by the trustee\n");
+	file = fopen("/etc/mtab", "r");
 
-	printf(" The access to a file ( directory) calculated on the following manner: the to masks - the first one for allow permission and the second one for deny. The first initially equals to [U], the second - to []. The path to the file (real path, not symbolic link) is analazied from the root directorry. If applicablee trustee is found, the approciate mask is ORed (or & ~(trustee mask) if C is set) to the mask in trustee. Access: given to superusers. Denied if deny flag set for at least one of the modes requested. Allowed if U flag is set, deny U flag is not set and unix permission is allow the access. Allow if all flags for requested mode are set. Denied otherwise. Known limitations: trustee system do not affect ioctl calls.");
-	exit(-1);
+	if (!file) return NULL;
 
-	
-}
-static void do_syscall(const struct trustee_command * command) {
-  int r;
-  /*  printf("Doing syscall for %s, dev is %x\n",command->filename,command->dev);
-      if (!command->dev) {printf("devname is %s\n",command->devname);}*/
-	r=set_trustee(command);
-	if (r) {
-		fprintf(stderr,"Can not do syscall for %s, code %x, reason %s\n",command->filename,r,strerror(errno));
+	x[9999] = '\0';
+	while (fgets(x, 9999, file)) {
+		if (strlen(x) == 9999) {
+			fprintf(stderr, "Error parsing mtab, line too long!\n");
+			return NULL;
+		}
+		
+		device = strtok(x, " ");
+		mount = strtok(NULL, " ");
+		fstype = strtok(NULL, " ");
+		options = strtok(NULL, " ");
+		num1 = strtok(NULL, " ");
+		num2 = strtok(NULL, " ");
+
+		if (!device || !mount || !fstype || !options || !num1 || !num2) {
+			fprintf(stderr, "Error parsing mtab, not enough fields!\n");
+			return NULL;
+		}
+
+		if (strcmp(fstype, "trusteesfs") == 0) {
+			return strdup(mount);
+		}
 	}
+
+	return NULL;
+}
+		
+void print_exit(void) {
+	printf("Usage: settrustees <options>\n");
+	printf("Options:\n");
+	printf("\n");
+	printf("    -f <config>\n");
+	printf("       Specify the trustees config file to use.\n");
+	printf("       Default: %s\n", trustee_config);
+	printf("    -D\n");
+	printf("       delete all trustees from the kernel and exit\n");
+	printf("    -d\n");
+	printf("       delete all trustees from the kernel before processing the trustees in\n");
+	printf("       the file\n");
+	printf("    -t <trustees device file>\n");
+	printf("       Specify the 'trustees' file from the mounted trusteefs fs\n");
+	printf("       This can often be automatically detected: %s\n", trustee_device);
+	printf("    -p <prefix>\n");
+	printf("       prefix to file names\n");
+	printf("\n");
+	printf("Description of the trustees config file format:\n");
+	printf("One path per line in this format:\n");
+	printf("    [block_device_name]/path/path:user_name:DCRWBE:+group_name:OtherChars\n");
+	printf("    {network_name}/path/path:user_name:DCRWBE:+group_name:OtherChars\n");
+	printf("A description of the various flags available:\n");
+	printf("    R - Read files permission\n");
+	printf("    W - Write permission\n");
+	printf("    B - Browse (like execute permission for directories)\n");
+	printf("    E - rEad diriectories\n");
+	printf("    X - eXecute (files, granted only if owner can execute the file\n");
+	printf("    U - use Unix permission system (set as default for /)\n");
+	printf("    D - deny the permissions in mask\n");
+	printf("    C - clear the permissions in mask\n");
+	printf("    O - only the directory and files in it (not subdirectories) affected by the\n");
+	printf("        trustee\n");
+	printf("    ! - All except user (group) affected by the trustee\n");
+	printf("The access to a file ( directory) calculated on the following manner: There are\n");
+	printf("two sets of masks: the deny masks and the allow masks.  Before the path\n");
+	printf("transversal begins, the deny mask is [] and the allow mask [U]. The path to a\n");
+	printf("file is transversed starting at the root of the partition.  At any point in the\n");
+	printf("transversal, if an applicable trustee is found, the bits in that mask are or'd\n");
+	printf("to the current mask.  If the 'C' flag is set, the bits are cleared instead of\n");
+	printf("set.  When the full path has been transversed and the masks have been\n");
+	printf("calculated if the mode requested is in the deny mask, the request will be\n");
+	printf("denied.  If the 'U' flag is set in the allow mask then next unix permissions\n");
+	printf("will be checked and possibly allowed access.  Next, if all flags for the\n");
+	printf("requested mode are in the allow mask, then the request will be granted.  If not\n");
+	printf("granted this point, the request will be denied.\n");
+	printf("\n");
+	printf("Limitations, notes and caveats:\n");
+	printf("Superuser will always be allowed access.\n");
+	printf("Hard links are partially disabled while trustees is loaded since it poses a\n");
+	printf("major security risk.  Hard links will be allowed into the current directory\n");
+	printf("only.  If someone manages to make hard links while the trustees module is not\n");
+	printf("loaded into different directories, then it is possible that a user could use it\n");
+	printf("to gain additional rights to a file since the trustees module has no way to\n");
+	printf("check all locations that a file exists on a disk.\n");
+	exit(-1);
 }
 
 int main(int argc, char * argv[])
 {
-	
-	char * filename="/etc/trustee.conf", * prefix="";
-	int i,j;
-	char c;
-	int flush=0;
-	int exitafterflush=0;
-	FILE * f;
-	char name[PATH_MAX+NAME_MAX]="";
+	char *prefix = "";
+	int i, j, r;
+	int flush = 0, exitafterflush = 0;
+	FILE *f;
+	char name[PATH_MAX + NAME_MAX] = "";
+	char  devname[PATH_MAX+NAME_MAX];
 	struct trustee_command command;
 	char s[32000];
-	char  devname[PATH_MAX+NAME_MAX];
-	char * olds, *n;
-	struct group * g;
-	struct passwd * pw; 
-	int isstdin=0;
+	char *olds, *n;
+	struct group *g;
+	struct passwd *pw; 
 	struct stat st;
+	int line;
   
-	command.filename=name;
-	command.devname=devname;
+	command.filename = name;
+	command.devname = devname;
 
-	while ((j=getopt(argc,argv,"t:f:dDp:"))!=EOF) {
+	trustee_device = determine_trustees_mount();
+	if (trustee_device) {
+		char *new;
+		new = realloc(trustee_device, strlen(trustee_device) + 100);
+		if (!new) {
+			fprintf(stderr, "It seems we ran out of memory... bailing...\n");
+			exit(-1);
+		}
+		trustee_device = new;
+		strcat(trustee_device, "/trustees");
+	} 
+
+	while ((j = getopt(argc,argv,"t:f:dhDp:"))!=EOF) {
 		switch (j) {
+		case 'h':
 		case '?': 
 			print_exit();
 		case 'd' : 
-			flush=1;
+			flush = 1;
 			break;
 		case 'D':
-			flush=1;
-			exitafterflush=1;
+			flush = 1;
+			exitafterflush = 1;
 			break;
 		case 'f':
-			filename=strdup(optarg);
+			trustee_config = strdup(optarg);
 			break;
 		case 't':
 			trustee_device = strdup(optarg);
 			break;
 		case 'p':
-			prefix=strdup(optarg);
+			prefix = strdup(optarg);
 			break;
 		}
-
-
 	}
 
-	f = fopen(trustee_device,"r");
+	if (!trustee_device) {
+		fprintf(stderr, "Couldn't determine where the trusteesfs was mounted.  You need to\n");
+		fprintf(stderr, "do something like 'mount -t trusteesfs none /place/to/mount' and\n");
+		fprintf(stderr, "run again.  It is possible the mount point could just not be determined\n");
+		fprintf(stderr, "in which case you should specify it with the -t option.\n");
+		exit (-1);
+	}
+		
 
-	if (f==NULL)
+	f = fopen(trustee_device, "w");
+	if (f == NULL)
 	{
-		fprintf(stderr, "Try specifying the trustees mount file with -t\n");
-		fprintf(stderr, "See -h for more information\n");
-		exit (0);
+		fprintf(stderr, "Could not open the trustees device for opening: %s\n", 
+		  trustee_device);
+		fprintf(stderr, "The error was %s\n", strerror(errno));
+		exit (-1);
 	}
 
 	if (!exitafterflush) {
-		isstdin=!strcmp(filename,"-");
-		if (isstdin) f=stdin; else {
-			f=fopen(filename,"r");
-			if (f==NULL) {
-				printf("Can not read file %s, reason %s\n", filename, strerror(errno));
+		if (strcmp(trustee_config, "-") == 0) { 
+			f = stdin;
+		} else {
+			f = fopen(trustee_config, "r");
+			if (!f) {
+				fprintf(stderr, "Could not read config file %s, reason %s\n", 
+				  trustee_config, strerror(errno));
 				exit(-1);
 			}
 		}
 	}
 	
 	if (flush) {
-	  command.command=TRUSTEE_COMMAND_REMOVE_ALL;
-	  do_syscall(&command);
-	}
-	if (exitafterflush) exit(0);
-	i=strlen(prefix);
-	if ((i>0) && (prefix[i-1]=='/')) prefix[i-1]=0;
-	while (fgets(s,32000,f)!=NULL) {
-		i=strlen(s);
-		if (i==0) continue;
-		if (s[0]=='#') continue;
-		if (s[i-1]=='\n') s[i-1]=0;
-		if (strlen(s)==0) continue;
-		if (*s=='[') {
-		  /*block device */;
-		  for (olds=s+1;(*olds!=']') && *olds;olds++);
-		  if (!*olds) {
-		    printf("] expected in %s",s);
-		    continue;
-		  }
-		  *olds=0;
-		  if (stat(s+1,&st)) {
-		    printf("Can not find device %s",s+1);
-		    continue;
-		  }
-		  if (!S_ISBLK(st.st_mode)) {
-		    printf("%s is not a block device, skipped\n",s+1);
-		    continue;
-		  }
-		  command.dev=st.st_rdev;
-		} else if (*s=='{') {
-		  /*network device */;
-		  for (olds=s+1;(*olds!='}') && *olds;olds++);
-		  if (!*olds) {
-		    printf("} expected in %s",s);
-		    continue;
-		  }
-		  *olds=0;
-		  command.dev=0;
-		  strcpy(command.devname,s+1);
-		} else {
-		  printf("Can not recognize line %s\n",s);
-		  continue;
+		command.command = TRUSTEE_COMMAND_REMOVE_ALL;
+		r = set_trustee(&command);
+		if (r) {
+			fprintf(stderr, "Can't set trustee for %s, reason: %s\n",
+			  command.filename, strerror(r));
+			exit(-1);
 		}
-		olds++;
-		
-		if ((n=strsep(&olds,":"))==NULL) {
-			fprintf(stderr,"Can not extract file name from string %s",s);
+	}
+
+	if (exitafterflush) exit(0);
+
+	i = strlen(prefix);
+	
+	if ((i > 0) && (prefix[i - 1] == '/')) 
+		prefix[i - 1] = 0;
+	
+	
+	s[31999] = '\0';
+	line = 0;
+	while (fgets(s, 31999, f) != NULL) {
+		line++;
+		i = strlen(s);
+		if (i == 31999) { /* handle lines longer than 31999 characters */
+			fprintf(stderr, "Warning skipping line longer than 31999 characters!\n");
+			while (((j = fgetc(f)) != '\n') && (j != EOF));
 			continue;
 		}
-		strcpy(name,prefix);
-		strcat(name,n); 
-		i=strlen(name);
-		if ((i>1) && (name[i-1]=='/')) name[i-1]=0;
-		while (((n=strsep(&olds,":"))!=NULL)) {
+		if (i == 0) continue;
+		if (s[0] == '#') continue;
+		if (s[i - 1] == '\n') s[i - 1] = 0;
+		if (strlen(s) == 0) continue;
+/*handle block device */;
+		if (*s == '[') {
+			for (olds = s + 1; (*olds != ']') && *olds; olds++);
+			if (!*olds) {
+				fprintf(stderr, "] expected on line %d\n", line);
+				continue;
+			}
+			*olds = 0;
+			if (stat(s + 1, &st)) {
+				fprintf(stderr, "Can not find device %s\n", s + 1);
+				continue;
+			}
+			if (!S_ISBLK(st.st_mode)) {
+				fprintf(stderr, "%s is not a block device, skipped\n", 
+				  s + 1);
+				continue;
+			}
+			command.dev=st.st_rdev;
+/*handle network device */;
+		} else if (*s == '{') {
+			for (olds = s + 1; (*olds != '}') && *olds; olds++);
+			if (!*olds) {
+				fprintf(stderr, "} expected on line %d\n", line);
+				continue;
+			}
+			*olds = 0;
+			command.dev = 0;
+			strcpy(command.devname, s+1);
+		} else {
+			fprintf(stderr, "Can not recognize line %s (line %d)\n", s, line);
+			continue;
+		}
+		olds++;
+
+/* pull in the filename */
+		if ((n = strsep(&olds,":"))==NULL) {
+			fprintf(stderr, "Can not extract file name from line %d\n", line);
+			continue;
+		}
+
+		strcpy(name, prefix);
+		strcat(name, n); 
+		i = strlen(name);
+		if ((i > 1) && (name[i-1] == '/')) 
+			name[i-1]=0;
+		while (((n = strsep(&olds, ":")) != NULL)) {
 			command.permission.mask=0;
-			if (n[0]==0) {
-				fprintf(stderr,"Can not extract user name from string %s",s);
+			if (n[0] == 0) {
+				fprintf(stderr, "Can not extract user name from line %d\n", line);
 				break;
 			}
-			if (n[0]=='+'){
-				command.permission.mask|= TRUSTEE_IS_GROUP_MASK;
+			if (n[0] == '+') {
+				command.permission.mask |= TRUSTEE_IS_GROUP_MASK;
 				n++;
 			}
-			if (n[0]==0) {
-				fprintf(stderr,"Can not extract user name from string %s",s);
+			if (n[0] == 0) {
+				fprintf(stderr, "Can not extract user name from line %d\n", line);
 				strsep(&olds,":");
 				break;
 			}
 			if (command.permission.mask & TRUSTEE_IS_GROUP_MASK) {
-				if ((g=getgrnam(n))==NULL) {
-					fprintf(stderr,"Invalid group %s",n);
-					strsep(&olds,":");
+				if ((g = getgrnam(n)) == NULL) {
+					fprintf(stderr, "Invalid group %s on line %d\n", n, line);
+					strsep(&olds, ":");
 					break;
 				}
 				command.permission.u.gid=g->gr_gid;
 			} else {
-				if (n[0]=='*') 
-				    command.permission.mask|=TRUSTEE_ALL_MASK;
-				else if ((pw=getpwnam(n))==NULL) {
-					fprintf(stderr,"Invalid user %s",n);
-					strsep(&olds,":");
+				if (n[0] == '*') 
+					command.permission.mask |= TRUSTEE_ALL_MASK;
+				else if ((pw = getpwnam(n)) == NULL) {
+					fprintf(stderr, "Invalid user %s on line %d\n", n, line);
+					strsep(&olds, ":");		
 					break;
-				} else 
-				  command.permission.u.uid=pw->pw_uid;
+				} else {
+					command.permission.u.uid = pw->pw_uid;
+				}
 			}
-			if ((n=strsep(&olds,":"))==NULL) {
-				fprintf(stderr,"Can not extract mask frm %s",s);
+
+			if ((n = strsep(&olds, ":")) == NULL) {
+				fprintf(stderr, "Can not extract mask from line %d\n", line);
 				break;
 			}
-			
-			for (;*n;n++) {
-				switch (*n) {
-				case 'D' :
-					command.permission.mask|=TRUSTEE_ALLOW_DENY_MASK;
-					break;
-				case 'C' :
-					command.permission.mask|=TRUSTEE_CLEAR_SET_MASK;
-					break;
-				case 'R':
-					command.permission.mask|=TRUSTEE_READ_MASK;
-					break;
-				case 'W':
-					command.permission.mask|=TRUSTEE_WRITE_MASK;
-					break;
-				case 'B':
-					command.permission.mask|=TRUSTEE_BROWSE_MASK;
-					break;
-				case 'E':
-				       command.permission.mask|=TRUSTEE_READ_DIR_MASK;
-					break;
-				case 'U':
-					command.permission.mask|=TRUSTEE_USE_UNIX_MASK;
-					break;
-				case 'X':
-					command.permission.mask|=TRUSTEE_EXECUTE_MASK;
-					break;
-				case '!':
-					command.permission.mask|=TRUSTEE_NOT_MASK;
-					break;
-				case 'O':
-					command.permission.mask|=TRUSTEE_ONE_LEVEL_MASK;
-					break;
-				
-				default:
-					fprintf(stderr,"Ilegal mask '%c' in string %s",*n,s);
+
+			for (; *n; n++) {
+				switch(*n) {
+					case 'D' :
+						command.permission.mask |= TRUSTEE_ALLOW_DENY_MASK;
+						break;
+					case 'C' :
+						command.permission.mask |= TRUSTEE_CLEAR_SET_MASK;
+						break;
+					case 'R':
+						command.permission.mask |= TRUSTEE_READ_MASK;
+						break;
+					case 'W':
+						command.permission.mask |= TRUSTEE_WRITE_MASK;
+						break;
+					case 'B':
+						command.permission.mask |= TRUSTEE_BROWSE_MASK;
+						break;
+					case 'E':
+						command.permission.mask |= TRUSTEE_READ_DIR_MASK;
+						break;
+					case 'U':
+						command.permission.mask |= TRUSTEE_USE_UNIX_MASK;
+						break;
+					case 'X':
+						command.permission.mask |= TRUSTEE_EXECUTE_MASK;
+						break;
+					case '!':
+						command.permission.mask |= TRUSTEE_NOT_MASK;
+						break;
+					case 'O':
+						command.permission.mask |= TRUSTEE_ONE_LEVEL_MASK;
+						break;
+					default:
+						fprintf(stderr, "Ilegal mask '%c' on line %d\n", *n, line);
 				}
-					
 			}
-			command.command=TRUSTEE_COMMAND_ADD;
-			do_syscall(&command);
-				
-			
+			command.command = TRUSTEE_COMMAND_ADD;
+			r = set_trustee(&command);
+			if (r) {
+				fprintf(stderr, "Can't set trustee for %s, reason: %s\n",
+				  command.filename, strerror(r));
+			}
 		}
 	}
-	if (!isstdin) fclose(f);
+	fclose(f);
 		
-	
 	return 0; 
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
